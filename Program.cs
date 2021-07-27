12e6a96e9e0cd657e11ef9ae8de62a35bdf12abe
Ricardo Filipe Marinho Braz <47540686+RicardoMarinho@users.noreply.github.com>
2021-07-27 15:28:39 +0100

    Add: Views and appsetings

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using ProjetoTI3.DomainModels;
+using ProjetoTI3.Models;
+
+namespace ProjetoTI3
+{
+    public class Program
+    {
+        public static string _conexao = "";
+        public static string _uidDefault = "00000000-0000-0000-0000-000000000000";
+        public static string _passwordDefault = "Password.01";
+
+        public static Colaborador getSessionColaborador(string uid)
+        {
+            Colaborador_Helper ch = new Colaborador_Helper(_conexao);
+            return ch.getConta(uid);
+        }
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProjetoTI3
+{
+    public class Startup
+    {
+        public IHostingEnvironment HostingEnvironment { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession(s => s.IdleTimeout = TimeSpan.FromMinutes(10));
+            services.AddMvc();
+        }
+        #region Construtor
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            this.HostingEnvironment = env;
+            this.Configuration = configuration;
+            Program._conexao = this.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+        }
+        #endregion
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc
+              (
+                routes =>
+                {
+                    routes.MapRoute(name: "default", template: "{controller=Colaborador}/{action=Login}/{id?}");
+                }
+             );
+        }
+    }
+}
